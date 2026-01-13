fb56ac06789e5eadcf1c0d8c6b4dfd89b357f3b7
anik33 <anik.cse.nu@gmail.com>
2026-01-13 20:16:31 +0600

    installed tailwind css,daisyui and created homepage layout

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1 className='text-blue-500'>This is home page content</h1>
+        </div>
+    );
+};
+
+export default HomePage;
